259b87d95a371128d2935593ec4d016a28cdce52
Softscope <63819603+Softscope@users.noreply.github.com>
2020-04-22 09:07:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
-
+  
+  // Adding EventLisntener to the Burger Menu in order to toggle it
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
+  });
+  
+  // Adding EventListener to the body and Links
+  document.addEventListener("click", (e) => {
+    if (!["fas", "burger"].some((el) => e.target.classList.contains(el))) {
+      nav.classList.remove("nav-active");
+    }
   });
 };
 
